25d5ffdcb78056abc9193fd08eba7206d915f2cf
OmarOuad <omar.ouadouha1@gmail.com>
2022-10-21 19:01:11 -0400

    Added signOut Button to the firstPage

--- app/src/main/java/com/example/akolapp/FirstPage.java
+++ app/src/main/java/com/example/akolapp/FirstPage.java
@@ -2,13 +2,35 @@ package com.example.akolapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FirstPage extends AppCompatActivity {
+    private Button SignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        SignOut = (Button) findViewById(R.id.SignOutButton);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                LogOut();
+
+            }
+        });
+
+    }
+    public void LogOut(){
+        Intent intent = new Intent(FirstPage.this,LoginPage.class);
+        startActivity(intent);
+
+
     }
 }
