248c51e8d7f3b4c5754e204dc984fa6ddaff6f36
tahatal7a <ttalh092@uottawa.ca>
2022-10-21 04:23:45 -0400

    changed the signup page java code

--- app/src/main/java/com/example/akolapp/SignUpPage.java
+++ app/src/main/java/com/example/akolapp/SignUpPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,73 +17,81 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpPage extends AppCompatActivity {
-    EditText FirstName, LastName, Email, Password;
+    EditText firstName, lastName, email, password;
     TextView sign;
     Button SignUpButton;
     FirebaseAuth mFirebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
 
         mFirebaseAuth=FirebaseAuth.getInstance();
-        Email=findViewById(R.id.Email);
-        Password=findViewById(R.id.Password);
-        FirstName=findViewById(R.id.FirstName);
-        LastName=findViewById(R.id.LastName);
+        email=findViewById(R.id.Email);
+        password=findViewById(R.id.Password);
+        firstName=findViewById(R.id.FirstName);
+        lastName=findViewById(R.id.LastName);
         SignUpButton=findViewById(R.id.SignUpButton);
         sign=findViewById(R.id.Sign);
-        SignUpButton.setOnClickListener((v) ->{
-                String email = Email.getText().toString();
-                String pwd = Password.getText().toString();
-                String firstname = FirstName.getText().toString();
-                String lastname = LastName.getText().toString();
 
-                if(!(firstname.isEmpty() && lastname.isEmpty() && email.isEmpty() && pwd.isEmpty())){
-                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(SignUpPage.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                startActivity(new Intent(SignUpPage.this, InfoCuisinier.class));
-                            } else{
-                                Toast.makeText(SignUpPage.this, "SignUp Unsuccessful, Please try again", Toast.LENGTH_SHORT).show();
-                            }
-                        }
 
-                    });
-                }
-                else if (firstname.isEmpty() || lastname.isEmpty() || email.isEmpty() || pwd.isEmpty()){
-                    Toast.makeText(SignUpPage.this,"Some Fields is Empty",Toast.LENGTH_SHORT).show();
-                    if (firstname.isEmpty()){
-                        FirstName.setError("Please enter a First Name");
-                        FirstName.requestFocus();
-                    }
-                    if(lastname.isEmpty()){
-                        LastName.setError("Please enter a Last Name");
-                        LastName.requestFocus();
-                    }
-                    if(email.isEmpty()){
-                        Email.setError("Please enter a Email Name");
-                        Email.requestFocus();
-                    }
-                    if(pwd.isEmpty()){
-                        Password.setError("Please enter a Password Name");
-                        Email.requestFocus();
-                    }
-                }
+        if(mFirebaseAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(),LoginPage.class));
+            finish();
+        }
 
-                    else{
-                        Toast.makeText(SignUpPage.this,"Error Occurred!!",Toast.LENGTH_SHORT).show();
-                    }
-                });
-        sign.setOnClickListener(new View.OnClickListener() {
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SignUpPage.this,LoginPage.class);
-                startActivity(i);
+                String mEmail = email.getText().toString().trim();
+                String pwd = password.getText().toString().trim();
+                String mFirst= firstName.getText().toString().trim();
+                String mLast= lastName.getText().toString().trim();
 
+            if(TextUtils.isEmpty(mEmail)){
+                email.setError("Email is required");
+                return;
+            }
+            if(TextUtils.isEmpty(pwd)){
+                password.setError("Password is required");
+                return;
+            }
+                if(TextUtils.isEmpty(mFirst)){
+                    firstName.setError("FirstName is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(mLast)){
+                    lastName.setError("LastName is required");
+                    return;
+                }
+            if(pwd.length()<6){
+                password.setError("Password must be 6 or more characters");
+                return;
+            }
+            mFirebaseAuth.createUserWithEmailAndPassword(mEmail,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(SignUpPage.this,"User Created",Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),InfoCuisinier.class));
+                    }
+                    else{
+                        Toast.makeText(SignUpPage.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
             }
         });
+
+        sign.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(SignUpPage.this,LoginPage.class);
+            startActivity(i);
+
         }
+    });
+}
     }
 
