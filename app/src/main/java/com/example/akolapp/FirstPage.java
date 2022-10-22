a523cbf9ad5a658d19229ac2695942c0861356d3
OmarOuad <omar.ouadouha1@gmail.com>
2022-10-21 23:50:04 -0400

    modified First page

--- app/src/main/java/com/example/akolapp/FirstPage.java
+++ app/src/main/java/com/example/akolapp/FirstPage.java
@@ -1,22 +1,82 @@
 package com.example.akolapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FirstPage extends AppCompatActivity {
     private Button SignOut;
+    private TextView welcomeTxt;
+    private FirebaseAuth auth;
+    private String currID;
+    private FirebaseFirestore storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        auth = FirebaseAuth.getInstance();
+        storage = FirebaseFirestore.getInstance();
         SignOut = (Button) findViewById(R.id.SignOutButton);
+        welcomeTxt = (TextView)findViewById(R.id.WelcomeText);
+        currID = auth.getUid();
+        DocumentReference docIdRef = storage.collection("cuisinier").document(currID);
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Document exists!");
+                        welcomeTxt.setText("Welcome, you are connected as a cuisinier");
+
+                    } else {
+                        Log.d(TAG, "Document does not exist!");
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
+
+                }
+            }
+        });
+        DocumentReference docIdRef2 = storage.collection("ClientUser").document(currID);
+        docIdRef2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "Document exists!");
+                        welcomeTxt.setText("Welcome, you are connected as a client");
+
+                    } else {
+                        Log.d(TAG, "Document does not exist!");
+                    }
+                } else {
+                    Log.d(TAG, "Failed with: ", task.getException());
+
+                }
+            }
+        });
+
+
+
+
         SignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,6 +85,7 @@ public class FirstPage extends AppCompatActivity {
 
             }
         });
+
 
     }
     public void LogOut(){
