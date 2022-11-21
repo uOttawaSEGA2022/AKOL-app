e08ff457d65afca46bafafda4458b5961a941162
[hibss61] <[hsoui030@uottawa.ca]>
2022-11-20 20:14:38 -0500

    Made PublishedRecipes and added some changes to it

--- app/src/main/java/com/example/akolapp/PublishedRecipes.java
+++ app/src/main/java/com/example/akolapp/PublishedRecipes.java
@@ -1,14 +1,44 @@
 package com.example.akolapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class PublishedRecipes extends AppCompatActivity {
+    FirebaseFirestore db;
+    Button MoveToMyRecipes,Modify,Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_published_recipes);
+
+        MoveToMyRecipes =  findViewById(R.id.MoveToMyRecipesButton);
+        Modify = findViewById(R.id.ModifyButton);
+        Back = findViewById(R.id.BackButton);
+        db = FirebaseFirestore.getInstance();
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
-}
+       }
