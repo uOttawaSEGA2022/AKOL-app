b6f36a286bc9647ae95fed69ae821e5555b1ca0a
[hibss61] <[hsoui030@uottawa.ca]>
2022-12-11 16:27:53 -0500

    Made PublishedRecipes and added some changes to it

---
+++ app/src/main/java/com/example/akolapp/PopularRecipesHRV.java
@@ -0,0 +1,14 @@
+package com.example.akolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopularRecipesHRV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popular_recipes_hrv);
+    }
+}
